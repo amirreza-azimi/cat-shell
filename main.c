23b6bf694bcece2828329d319991239b3ccc1e34
unknown-program <masihaazimi4@gmail.com>
2025-12-15 08:29:03 -0800

    Update main.c

--- main.c
+++ main.c
@@ -10,6 +10,46 @@ char cwd[MAX];
 char *history[MAX_HISTORY];
 int history_counts = 0;
 
+void run_command(char **argv) {
+    STARTUPINFOA si;
+    PROCESS_INFORMATION pi;
+    char cmdline[MAX] = "";
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    for (int i = 0; argv[i]; i++) {
+        strcat(cmdline, argv[i]);
+        strcat(cmdline, " ");
+    }
+
+    char full_command[MAX];
+    snprintf(full_command, MAX, "cmd /c %s", cmdline);
+
+    if (!CreateProcessA(
+        NULL,
+        full_command,
+        NULL,
+        NULL,
+        FALSE,
+        0,
+        NULL,
+        NULL,
+        &si,
+        &pi
+    )) {
+        printf("ERROR: Could not run command\n");
+        return;
+    }
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+}
+
+
+
 typedef struct {
     const char *name;
     void (*func)();
@@ -20,24 +60,26 @@ void cmd_hello() {
 }
 
 void cmd_clean() {
-    system("cls");
+    char *argv[] = {"cls", NULL};
+    run_command(argv);
 }
 
 void cmd_path() {
     printf("Your cat is located in: %s\n", cwd);
 }
 
-void cmd_food() {
-    printf("Here is all of your cat food: ");
-    system("dir");
+void cmd_foods() {
+    printf("Here is all of your cat foods: ");
+    char *argv[] = {"dir", NULL};
+    run_command(argv);
 }
 
 void cmd_help() {
-    printf("\nCat is here to help you!\n\nExclusive Commands:\n\n    1.hello -> you can just say hello to computer.\n    2.clean -> this is just cls.\n    3.whereismycat -> this is pwd but with more fun.\n    4.catfood -> showing folder and files like dir.\n\nYou can see a full list of not exclusive commands (cmd commands) in:\n\n    https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/windows-commands\n\n");
+    printf("\nCat is here to help you!\n\nExclusive Commands:\n\n    1.hey -> you can just say hello to computer.\n    2.clean -> this is just cls.\n    3.whereismycat -> this is pwd but with more fun.\n    4.catfood -> showing folder and files like dir.\n\nYou can see a full list of Exclusive Commands in:\n\n    https://www.catweb.com\n\nYou can see a full list of not exclusive commands (cmd commands) in:\n\n    https://learn.microsoft.com/en-us/windows-server/administration/windows-commands/windows-commands\n\n");
 }
 
 void cmd_version() {
-    printf("\nCat is in version 1.0.0.\n\n");
+    printf("\nCat is in version 1.1.0.\n\n");
 }
 
 void cmd_history() {
@@ -52,48 +94,24 @@ CustomCommand commands[] = {
     {"hey", cmd_hello},
     {"clean", cmd_clean},
     {"whereismycat", cmd_path},
-    {"catfood", cmd_food},
+    {"catfoods", cmd_foods},
     {"help", cmd_help},
     {"mycatversion", cmd_version},
     {"catmeows", cmd_history},
     {NULL, NULL},
 };
 
+int parse_input(char *input, char **argv) {
+    int argc = 0;
 
-
-void run_command(char *cmd) {
-    STARTUPINFOA si;
-    PROCESS_INFORMATION pi;
-
-    ZeroMemory(&si, sizeof(si));
-    si.cb = sizeof(si);
-    ZeroMemory(&pi, sizeof(pi));
-
-    char command[MAX];
-    strcpy(command, cmd);
-
-    char full_command[MAX];
-    snprintf(full_command, MAX, "cmd /c %s", command);
-
-    if(!CreateProcessA(
-        NULL,
-        full_command,
-        NULL,
-        NULL,
-        FALSE,
-        0,
-        NULL,
-        NULL,
-        &si,
-        &pi
-    )) {
-        printf("ERROR: Could not run '%s'\n", cmd);
-        return;
+    char *token = strtok(input, " ");
+    while (token != NULL && argc < 31) {
+        argv[argc++] = token;
+        token = strtok(NULL, " ");
     }
 
-    WaitForSingleObject(pi.hProcess, INFINITE);
-    CloseHandle(pi.hProcess);
-    CloseHandle(pi.hThread);
+    argv[argc] = NULL;
+    return argc;
 }
 
 int main() {
@@ -115,11 +133,28 @@ int main() {
     printf("\033[0m"); // reset
 
     while(1){
+
         GetCurrentDirectoryA(MAX, cwd);
         printf("caaaaaaaaaat> ");
         
         fgets(input, MAX, stdin);
         input[strcspn(input, "\n")] = '\0';
+        char *argv[64];
+        int argc = parse_input(input, argv);
+
+        if (argc == 0) continue;
+        
+        if (strcmp(argv[0], "cd") == 0) {
+            if (argc < 2) {
+                printf("cd: missing path\n");
+            } else {
+                SetCurrentDirectoryA(argv[1]);
+            }
+            continue;
+        }
+
+
+        if (strlen(input) == 0) continue;
 
         if(history_counts < MAX_HISTORY){
             history[history_counts] = strdup(input);
@@ -142,7 +177,7 @@ int main() {
         int i = 0;
         int found = 0;
         while(commands[i].name != NULL) {
-            if(strcmp(input, commands[i].name) == 0) {
+            if(strcmp(argv[0], commands[i].name) == 0) {
                 commands[i].func();
                 found = 1;
                 break;
@@ -151,19 +186,13 @@ int main() {
         }
 
         if(!found) {
-            run_command(input);
+            run_command(argv);
+        }
         }
     }
 
-
-        if(strncmp(input, "cd ", 3) == 0){
-            char *path = input + 3;
-            if(SetCurrentDirectoryA(path)){
-
-            } else {
-                printf("ERROR: Could not change directory to '%s'\n", path);
-            }
-        }
+    for (int i = 0; i < history_counts; i++) {
+        free(history[i]);
     }
 
     return 0;
@@ -171,5 +200,3 @@ int main() {
 
 
 // have a nice day or night or ... i don't know just bye.
-
-
